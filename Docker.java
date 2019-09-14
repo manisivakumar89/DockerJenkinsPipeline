437caac8ecd4b096b9c9b488eeba175d5ef5e998
Ubuntu <denversiva89@gmail.com>
2019-09-13 19:24:24 -0600

    Update Docker files

---
+++ Docker.java
@@ -0,0 +1,8 @@
+class Docker { 
+    public static void main(String args[]) 
+    { 
+        String s = "Nature"; 
+        s = s.concat("! is the best."); 
+        System.out.println(s); 
+    } 
+} 
---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM java:8
+COPY Docker.java .
+RUN javac Docker.java
+
+CMD ["java", "Docker"]
